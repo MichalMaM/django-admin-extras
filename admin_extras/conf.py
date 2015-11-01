40509545bc2217e4a0db73a298cf616db298f3b3
michal dub <michal.dub@sanomamedia.cz>
2015-11-01 22:02:50 +0100

    fixed typo in conf

--- admin_extras/conf.py
+++ admin_extras/conf.py
@@ -4,5 +4,5 @@ from django.conf import settings
 READONLY_CODENAME = getattr(settings, 'ADMIN_EXTRAS_READONLY_CODENAME', 'read_only')
 
 # set content types for auto register readonly admin
-# for all models use ADMIN_EXTRAS_READONLY_CODENAME = '__all__'
-AUTO_ADMIN_READONLY_CT = getattr(settings, 'ADMIN_EXTRAS_READONLY_CODENAME', ())
+# for all models use ADMIN_EXTRAS_AUTO_ADMIN_READONLY_CT = '__all__'
+AUTO_ADMIN_READONLY_CT = getattr(settings, 'ADMIN_EXTRAS_AUTO_ADMIN_READONLY_CT', ())
